89e8149b5d8b1106c751b9d4debd9486801c8e6d
조영희 <jhee0046@naver.com>
2024-07-13 15:49:25 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 372 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,22 @@
+import math 
+def isprime(x) :
+    for i in range(2, int(math.sqrt(x)+1)) :
+        if x % i == 0 :
+            return False
+    return True
+
+N = int(input())
+result = 0
+
+for i in range(N,1000001) :
+    if i == 1 :
+        continue
+    if str(i) == str(i)[::-1] :
+        if isprime(i) == True :
+            result = i
+            break
+
+if result == 0 :
+    result = 1003001
+    
+print(result)
