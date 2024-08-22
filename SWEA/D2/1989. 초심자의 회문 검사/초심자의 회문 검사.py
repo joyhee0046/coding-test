47cd6f7c1d6b9fe0848d32ec6bcf97cde234806e
조영희 <jhee0046@naver.com>
2024-08-22 09:09:54 +0900

    [D2] Title: 초심자의 회문 검사, Time: 93 ms, Memory: 43,656 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,11 +1,17 @@
-def palin(li) :
-    if len(li) < 2:
-        return 1
-    if li[0] != li[-1]:
-        return 0
-    return palin(li[1:-1])
+# import sys
+# sys.stdin = open("input.txt", "r")
 
+# 주어진 입력받기
 T = int(input())
-for tc in range(1,T+1) :
-    li = list(input())
-    print(f"#{tc} {palin(li)}")
+for tc in range(1, 1+T):
+    words = list(input())
+
+    ans = 1
+    # 맨 앞과 맨 뒤가 같은지 확인
+    for check in range(len(words)//2):
+        if words[check] != words[-(check+1)]:
+            ans = 0
+            break
+
+    # 정답 출력
+    print(f"#{tc} {ans}")
