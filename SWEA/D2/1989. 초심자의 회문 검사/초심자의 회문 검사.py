8e2239155726f353646af2e94206a6340c24836b
조영희 <jhee0046@naver.com>
2024-09-14 18:23:24 +0900

    [D2] Title: 초심자의 회문 검사, Time: 124 ms, Memory: 44,536 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,17 +1,41 @@
 # import sys
 # sys.stdin = open("input.txt", "r")
 
-# 주어진 입력받기
+# #그냥 풀기
+# T = int(input())
+#
+# for tc in range(1,T+1) :
+#     li = list(input())
+#     ans = 1
+#     for i in range(len(li)//2) :
+#         if li[i] == li[len(li)-i-1] :
+#             pass
+#         else :
+#             ans = 0
+#     print(f"#{tc} {ans}")
+
+# #함수 이용
+# def Palin(li) :
+#     ans =li[::-1]
+#     if li == ans :
+#         return 1
+#     else :
+#         return 0
+#
+# T = int(input())
+# for tc in range(1,T+1) :
+#     li = list(input())
+#     print(f"#{tc} {Palin(li)}")
+
+#재귀함수 이용
+def palin(li) :
+    if len(li) < 2:
+        return 1
+    if li[0] != li[-1]:
+        return 0
+    return palin(li[1:-1])
+
 T = int(input())
-for tc in range(1, 1+T):
-    words = list(input())
-
-    ans = 1
-    # 맨 앞과 맨 뒤가 같은지 확인
-    for check in range(len(words)//2):
-        if words[check] != words[-(check+1)]:
-            ans = 0
-            break
-
-    # 정답 출력
-    print(f"#{tc} {ans}")
+for tc in range(1,T+1) :
+    li = list(input())
+    print(f"#{tc} {palin(li)}")
