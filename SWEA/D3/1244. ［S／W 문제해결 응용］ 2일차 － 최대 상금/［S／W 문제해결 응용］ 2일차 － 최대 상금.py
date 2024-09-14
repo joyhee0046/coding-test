7ece1e9c59c8e94f02cda899f5ce1eb90cc299f7
조영희 <jhee0046@naver.com>
2024-09-14 18:17:44 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 1,756 ms, Memory: 52,200 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,40 +1,36 @@
 # import sys
 # sys.stdin = open('input.txt','r')
 
-def max_prize(borad, num):
-    global check_borad
-    # 변경횟수가 0번 남을때까지 반복
-    if num == 0:
-        # 더 큰 값 고르기
-        check_borad = max(check_borad, int(''.join(map(str, borad))))
-        return check_borad
 
-    for i in range(n):
-        # i랑 j조합 만들기
-        for j in range(i + 1, n):
-            # 맘대로 수정할 임시 보드 만들기
-            tmp_borad = borad[:]
-            # i, j조합으로 자리 바꾸기
-            tmp_borad[i], tmp_borad[j] = tmp_borad[j], tmp_borad[i]
-            # 바꿔준 위치와 남은 횟수가 이미 했던 검사면 지나가기
-            if (num, tmp_borad) not in visited:
-                # 재귀로 다음 카드 바꾸기.
-                max_prize(tmp_borad, num - 1)
-                # visited에 남은 교환횟수랑 지금 값 저장_ 중복 작업 방지
-                visited.append((num, tmp_borad))
+test_case = int(input())  #몇번 테스트 할거야?
 
-# 주어진 입력 받기
-T = int(input())
-for tc in range(1, 1 + T):
-    borad, num = map(str, input().split())
-    borad = list(int(i) for i in borad)
-    num = int(num)
-    n = len(borad)
+def search_prize(cards, change_num):   # 함수호출하면 시작되는 정의부분
+    if change_num == 0:   #변경횟수가 0번 남을때까지 반복
+        global max_prize  #밖에서 정의했던 맥스프라이즈 사용하겠다고 선언
+        max_prize = max(max_prize, int(''.join(map(str, cards)))) #맥스프라이즈는 max_prize와 카드리스트 중 더 큰 값으로 재정의한다.
+        return cards   #종료시점
 
-    check_borad = 0
-    visited = []
 
-    max_prize(borad, num)  # 함수 호출, 카드리스트와 교환횟수
+    for i in range(cards_num-1):  #카드갯수만큼 반복_range가 0부터 시작해서 -1
+        for j in range(i + 1, cards_num): #반복문으로 만드는 조합_0부터 시작하는데, 이번에는 시작부분에 +1로 갯수조정
+            tmp_cards = cards[:] #원본리스트를 복사하여 임시리스트 생성
+            tmp_cards[i], tmp_cards[j] = tmp_cards[j], tmp_cards[i]  #반복문을 통해서 선택한 카드 두 장 자리 바꾸기
+            if (change_num, tmp_cards) not in visited:  #만들어진 카드목록와 남은 교환횟수 세트가 이미 확인한 것과 같다면_다시 반복문으로 올라가기
+                # print(change_num, tmp_cards, visited, change_num)    #같지 않다면 아래로 진행. // 프린트로 잘 나오고 있는 지 확인
+                search_prize(tmp_cards, change_num - 1)   #재귀로 함수 다시 호출. 다음 카드 바꾸기 하려고.
+                visited.append((change_num, tmp_cards)) #확인했던 내용 비지티드에 추가하기_다음 탐색에서 확인할 수 있어야 하니까.
 
-    # 정답 출력
-    print(f"#{tc} {check_borad}")
+
+for t in range(test_case): # 테케만큼 반복
+    cards, change_num = input().split() #입력받기
+
+    cards = [int(num) for num in cards] #입력받은 문자열을 정수 리스트로 만들기_카드리스트
+    cards_num = len(cards) #카드갯수확인
+    change_num = int(change_num) #입력받은 문자열 정수로 만들기_몇번 교환할건지
+
+    max_prize = 0  #만들어진 가장 큰 수_갱신되도록 가장 작게 설정
+    visited = []   #이미 확인한 카드 목록
+
+    search_prize(cards, change_num)   #서치프라이즈 함수 호출, 카드리스트와 교환횟수
+
+    print(f"#{t + 1} {max_prize}")  #형식에 맞춰서 출력
